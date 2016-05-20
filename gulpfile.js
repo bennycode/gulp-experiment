8def8f5a99552edbe9c5332aa69c55141ade3ef3
Benny Neugebauer <bn@bennyn.de>
2016-05-20 15:01:05 +0200

    Added "gulp ts"

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,12 @@ var plugins = require('gulp-load-plugins')();
 gulp.task('default', function () {
   plugins.util.log(plugins.util.colors.magenta('Hello'), 'World!');
 });
+
+gulp.task('ts', function () {
+  return gulp.src('src/**/*.ts')
+    .pipe(plugins.typescript({
+      noImplicitAny: true,
+      out: 'output.js'
+    }))
+    .pipe(gulp.dest('dest/scripts'));
+});
