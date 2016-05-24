475828f2d5dccf893ac8b1f157e267d6285ceab3
Benny Neugebauer <bn@bennyn.de>
2016-05-24 13:33:28 +0200

    Added static server, live reload & excluded content from source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var path = require('path');
-var plugins = require('gulp-load-plugins')();
+var plugins = require('gulp-load-plugins')({pattern: '*'});
 var tsProject = plugins.typescript.createProject('tsconfig.json');
+var browserSync = plugins.browserSync.create();
 
 gulp.task('default', function () {
+  browserSync.init({
+    server: {baseDir: './'},
+    port: 3636,
+    startPath: '/dest/Greeter/index.html'
+  });
+
   gulp.watch('src/**/*.ts').on('change', function (file) {
     var pathObject = path.parse(file.path);
     plugins.util.log('Compiling', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'...');
@@ -12,9 +19,10 @@ gulp.task('default', function () {
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.typescript(tsProject))
       .pipe(plugins.uglify())
-      .pipe(plugins.sourcemaps.write('.', {includeContent: true, sourceRoot: ''}))
+      .pipe(plugins.sourcemaps.write('.', {includeContent: false, sourceRoot: '/src'}))
       .pipe(gulp.dest('dest'));
 
     plugins.util.log('Compiled', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'');
+    browserSync.reload();
   });
 });
