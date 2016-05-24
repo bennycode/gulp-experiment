549d6bf54bff947e1d81c369256817e8d5371313
Benny Neugebauer <bn@bennyn.de>
2016-05-24 16:40:07 +0200

    Listening to streams and outsourcing functions

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,34 @@ var plugins = require('gulp-load-plugins')({pattern: '*'});
 var tsProject = plugins.typescript.createProject('tsconfig.json');
 var browserSync = plugins.browserSync.create();
 
-gulp.task('default', function () {
-  browserSync.init({
-    port: 3636,
-    server: {baseDir: './'},
-    startPath: '/' + paths.output
-  });
-
-  // HTML
-  gulp.watch(paths.output + '/**/*.html').on('change', browserSync.reload);
-
-  // TypeScript
-  gulp.watch(paths.input + '/**/*.ts').on('change', function (file) {
+var actions = {
+  compileTypeScript: function (file) {
     var pathObject = path.parse(file.path);
     plugins.util.log('Compiling', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'...');
 
-    gulp.src(file.path, {base: paths.input})
+    var stream = gulp.src(file.path, {base: paths.input})
       .pipe(plugins.sourcemaps.init())
       .pipe(plugins.typescript(tsProject))
       .pipe(plugins.uglify())
       .pipe(plugins.sourcemaps.write('.', {includeContent: false, sourceRoot: '/' + paths.input}))
       .pipe(gulp.dest(paths.output));
 
-    plugins.util.log('Compiled', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'');
-    browserSync.reload();
-  });
+    stream.on('end', function () {
+      plugins.util.log('Compiled', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'');
+      browserSync.reload();
+    });
+  },
+  startServer: function () {
+    browserSync.init({
+      port: 3636,
+      server: {baseDir: './'},
+      startPath: '/' + paths.output
+    });
+  }
+};
+
+gulp.task('default', function () {
+  actions.startServer();
+  gulp.watch(paths.input + '/**/*.ts').on('change', actions.compileTypeScript);
+  gulp.watch(paths.output + '/**/*.html').on('change', browserSync.reload);
 });
