79eaa9b4c1053ad0c45c6a0bacee22e6c882272e
Benny Neugebauer <bn@bennyn.de>
2016-05-24 17:26:59 +0200

    Code cleanup (chore)

--- gulpfile.js
+++ gulpfile.js
@@ -23,28 +23,27 @@ var actions = {
   }
 };
 
+actions.watchTypeScript = function (file) {
+  var pathObject = path.parse(file.path);
+  plugins.util.log('Compiling', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'...');
+
+  var stream = gulp.src(file.path, {base: paths.input}).pipe(gulp.dest(paths.output));
+
+  stream.on('end', function () {
+    plugins.util.log('Compiled', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'');
+    browserSync.reload();
+  });
+
+  actions.compileTypeScript(stream);
+};
+
 gulp.task('default', ['init'], function () {
   actions.startServer();
-  gulp.watch(paths.input + '/**/*.ts').on('change', function (file) {
-    var pathObject = path.parse(file.path);
-    plugins.util.log('Compiling', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'...');
-
-    var stream = gulp.src(file.path, {base: paths.input})
-      .pipe(gulp.dest(paths.output));
-
-    stream.on('end', function () {
-      plugins.util.log('Compiled', '\'' + plugins.util.colors.yellow(pathObject.base) + '\'');
-      browserSync.reload();
-    });
-
-    actions.compileTypeScript(stream);
-  });
+  gulp.watch(paths.input + '/**/*.ts').on('change', actions.watchTypeScript);
   gulp.watch(paths.output + '/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('init', function () {
-  plugins.util.log('Pre-compiling all files...');
-
-  return actions.compileTypeScript(gulp.src(paths.input + '/**/*.ts'))
-    .pipe(gulp.dest(paths.output));
+  plugins.util.log('Pre-compiling files...');
+  return actions.compileTypeScript(gulp.src(paths.input + '/**/*.ts')).pipe(gulp.dest(paths.output));
 });
